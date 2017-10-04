6b875c03f5aaa26d50a29124a84032717694d909
Sergii Melnyk <sergii.melnyk@mev.com>
2017-10-04 19:03:19 +0300

    fix error

--- configs/gulpfile.js
+++ configs/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
  
 gulp.task('minify', function() {
-  return gulp.src('*.html')
+  return gulp.src('../*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('/var/www/mysite'));
 });
